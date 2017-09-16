d5a4f66797bdf4aad97750bc266192d13b37b73a
squeemishly <squeemishly@gmail.com>
2017-09-16 16:46:01 -0600

    finds the id and token for logged in user and starts axios call

--- src/lib/user-workouts.js
+++ src/lib/user-workouts.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 // import { Link } from 'react-router-dom'
 
 class UserWorkouts extends Component {
+  componentDidMount() {
+    const { id, token } = JSON.parse(localStorage.getItem('userInfo'))
+    axios.get(`http://localhost:3000/api/v1/users/${id}/workouts`)
+    .then( data => {
+      console.log(data.data)
+    })
+  }
+
   render() {
     return (
       <div>
