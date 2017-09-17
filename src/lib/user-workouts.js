c0544b8e1360d17a779d0570a534a725e1e4c09c
squeemishly <squeemishly@gmail.com>
2017-09-16 20:29:29 -0600

    creates constructor method and sets API call response to a state array

--- src/lib/user-workouts.js
+++ src/lib/user-workouts.js
@@ -4,11 +4,24 @@ import axios from 'axios'
 // import { Link } from 'react-router-dom'
 
 class UserWorkouts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allWorkouts: []
+    }
+  }
+
   componentDidMount() {
     const { id, token } = JSON.parse(localStorage.getItem('userInfo'))
-    axios.get(`http://localhost:3000/api/v1/users/${id}/workouts`)
-    .then( data => {
-      console.log(data.data)
+    axios.get(`http://localhost:3000/api/v1/users/${id}/workouts`, {
+      params: {
+        token: token
+      }
+    })
+    .then( workouts => {
+      this.setState({
+        allWorkouts: workouts.data
+      })
     })
   }
 
