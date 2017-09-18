dd52a8607250423fd8749756d33eadcea434c902
squeemishly <squeemishly@gmail.com>
2017-09-17 18:18:53 -0600

    adds link to Logout.js and clicking on Logout now routes user to the logout page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Link
 } from 'react-router-dom'
 import Login from './lib/login'
+import Logout from './lib/logout'
 import NewUser from './lib/new-user'
 import Profile from './lib/profile'
 import UserWorkouts from './lib/user-workouts'
@@ -18,12 +19,13 @@ const App = () => (
         <ul>
           <li><Link to='#'>My Workouts</Link></li>
           <li><Link to='#'>All Lifts</Link></li>
-          { localStorage.getItem('userInfo') && <li><Link to='/'>Logout</Link></li> }
+          { localStorage.getItem('userInfo') && <li><Link to='/logout'>Logout</Link></li> }
           { !localStorage.getItem('userInfo') && <li><Link to='/'>Sign Up or Login</Link></li> }
         </ul>
       </nav>
 
       <Route exact path="/" component={ Login }/>
+      <Route path="/logout" component={ Logout }/>
       <Route path="/new-user" component={ NewUser }/>
       <Route path="/profile" component={ Profile }/>
       <Route path="/user-workouts" component={ UserWorkouts }/>
